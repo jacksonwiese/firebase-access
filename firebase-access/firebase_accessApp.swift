4631fe522be3efdb0c9eb6b77bc9043b5fa34b23
jacksonwiese <jacksonwiese1@gmail.com>
2023-04-12 14:32:53 -0500

    Initial Commit

---
+++ firebase-access/firebase_accessApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebase_accessApp.swift
+//  firebase-access
+//
+//  Created by Jackson Wiese on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct firebase_accessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
