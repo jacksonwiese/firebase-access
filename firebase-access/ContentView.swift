39e78f9c543abff70c37e82b7b351249d6981059
jacksonwiese <jacksonwiese1@gmail.com>
2023-04-17 15:24:32 -0500

    Updates

--- firebase-access/ContentView.swift
+++ firebase-access/ContentView.swift
@@ -9,19 +9,47 @@ import SwiftUI
 import Firebase
 
 struct ContentView: View {
+    
     @ObservedObject var model = ViewModels()
     @State var id = ""
     @State var category = ""
     @State var time = ""
-        
+    
     var body: some View {
         //excellent for settings page
-    
-        List(model.list) {//list of model classes / ids categories
-            item in
-            
-            Text(item.category)
+        
+        VStack{
+            List(model.list) {//list of model classes / ids categories
+                item in
+                
+                Text(item.category)
+            }
         }
+        
+        Divider()
+        
+        //for elements the user wants to add
+        VStack(spacing: 5) {
+            TextField("ID: ", text:$id)
+                .keyboardType(.numberPad)
+            TextField("category: ", text:$category)
+            TextField("time: ", text:$time)
+            
+            Button(action: {
+                //call app data
+                model.addData(id: Int(id) ?? 0, category: category, time: time)
+                
+                //clear field
+                id = ""
+                category = ""
+                time = ""
+                
+            }) {
+                Text("Upload")
+            }
+            
+        }
+        
     }//var body ends here
     
     init() {
